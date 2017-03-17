949e2edcfd45b21339704132a9eb5ee2b8c7c1f7
ZuMingShen <gnshenzuming@gmail.com>
2017-03-17 17:03:09 +0800

    It is a new python algorithm(Add Two Numbers)

--- Leetcode/2.Add Two Numbers.py
+++ Leetcode/2.Add Two Numbers.py
@@ -6,35 +6,30 @@
 # software  :PyCharm
 
 
-# class ListNode(object):           #Definition for singly-linked list
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):           #Definition for singly-linked list
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
-def addTwoNumbers(self, l1, l2):
-    """
-    :param self:
-    :param l1: ListNode
-    :param l2: ListNode
-    :return: ListNode
-    """
-    if l1 == None:
-        return l2
-    if l2 == None:
-        return l1
-
-
-def listTolong(self, l1, l2):
-    num1, num2 = 0, 0
-    temp1, temp2 = 1, 1
-    for i in range(len(l1)):
-        num1 += l1[i]
-        temp1 *= 10
-    for j in range(len(l2)):
-        num2 += l2[j]
-        temp2 *= 10
-    num3 = num1 + num2
-    lists = map(int, str([num3][0]))
-    return lists
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry%10)
+            cur = cur.next
+            carry //= 10
+        return dummy.next
 
